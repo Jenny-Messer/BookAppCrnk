842c79341f48d0c31ade463b0bd61554daed761c
jennifermesser <jennifer.messer@travelex.com>
2018-08-06 15:07:39 +0100

    created project

---
+++ src/main/java/dfgvhbj/BookApp.java
@@ -0,0 +1,33 @@
+package dfgvhbj;// Copyright (c) 2018 Travelex Ltd
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BookApp {
+
+    public static void main(String... args) {
+        SpringApplication.run(BookApp.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+
+}
